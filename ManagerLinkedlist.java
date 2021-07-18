2597471aa47c6f381fd43b0a9a5d0e2eef74c986
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-19 01:09:13 +0530

    UC3-Append 30and 70 to 56

---
+++ ManagerLinkedlist.java
@@ -0,0 +1,12 @@
+package com.linkedList;
+
+public class ManagerLinkedlist {
+	public static void main(String[] args) {
+		SimpleLinkedList<Integer> numberList = new SimpleLinkedList<Integer>();
+		numberList.add(30);
+		numberList.addAtStart(56);
+		numberList.add(70);
+		numberList.show();
+	}
+	
+	}
