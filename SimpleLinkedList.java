047238e17d9a4216e649556c7fe49a2524cfe599
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-19 01:42:59 +0530

    UC8- added search method and then able to add

---
+++ SimpleLinkedList.java
@@ -0,0 +1,125 @@
+package com.linkedList;
+
+
+public class SimpleLinkedList<T> {
+	Node<T> head;
+
+	/**
+	 * add elements to the linked list
+	 * 
+	 * @param data
+	 */
+	public void add(T data) {
+		Node<T> newNode = new Node<>();
+		newNode.data = data;
+		if (head == null) {
+			head = newNode;
+		} else {
+			Node<T> node = head;
+			while (node.next != null) {
+				node = node.next;
+			}
+			node.next = newNode;
+		}
+	}
+	
+
+	/**
+	 * Inserts the element at the starting of the linked list
+	 * 
+	 * @param data
+	 */
+	public void addAtStart(T data) {
+		Node<T> newNode = new Node<>();
+		newNode.data = data;
+		newNode.next = head;
+		head = newNode;
+	}
+
+	
+	/**
+	 * Current node should not be null and it is pointing to the next should be null
+	 */
+	public void show() {
+		Node<T> node = head;
+		while (node.next != null) {
+			System.out.println(node.data);
+			node = node.next;
+		}
+		System.out.println(node.data);
+	}
+	
+	
+	/**
+	 * Inserts the specific element in between of two elements
+	 * @param index
+	 * @param data
+	 */
+	public void addAtIndex(int index, T data) {
+		Node<T> newNode = new Node<>();
+		newNode.data = data;
+
+		if (index == 0) {
+			addAtStart(data);
+		} else {
+			Node<T> node = head;
+			for (int i = 0; i < index - 1; i++) {
+				node = node.next;
+			}
+			newNode.next = node.next;
+			node.next = newNode;
+		}
+	}
+	
+	
+	/**
+	 * It removes the first element present in the Linked list
+	 */
+	public void pop() {
+		if(null != head) {
+			Node<T> newHead = head.next;
+			head = newHead;
+		}
+	}
+	
+	
+	/**
+	 *It removes the last element present in the Linked List 
+	 */
+	public void popLast() {
+		Node<T> node = head;
+		Node<T> secondlastNode = head;
+		while(node.next != null) {
+			secondlastNode = node;
+			node = node.next;
+		}
+		secondlastNode.next = null;
+	}
+	
+	
+	/**
+	 * Checks key value is present or not and if present
+	 * at which position
+	 * @param data
+	 * @return the index at which the key value is found 
+	 */
+	public int searchWithValue(T data) {
+		Node<T> node = head;
+		int index = 0;
+		if(node != null) {
+			while((node.next != null) || (node.data != null)) {
+				if(node.data == data) {
+					break;
+				}
+				node = node.next;
+				if(node == null) {
+					return -1;
+				}
+				index++;
+			}
+		}
+		return index;
+	}
+
+
+}
