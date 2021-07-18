27e594b1ee5b30c8ff718c94143566e64a1dcca9
Rahu206 <81007551+Rahu206@users.noreply.github.com>
2021-07-18 14:43:50 +0530

    UC2-able to create linked list

---
+++ ManagerLinkedlist.java
@@ -0,0 +1,17 @@
+package com.linkedList;
+
+import java.util.LinkedList;
+
+public class ManagerLinkedlist {
+	public static void main(String[] args) {
+		SimpleLinkedList siLinkedList = new SimpleLinkedList();
+		siLinkedList.insertFirst(70);
+		siLinkedList.insertSecond(30);
+		
+		siLinkedList.insertLast(56);
+				System.out.println();
+		
+		
+	}
+
+}
